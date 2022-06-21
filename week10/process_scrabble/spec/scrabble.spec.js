88b5889dd2b8019e921bab8c749f095353c498f9
Sam Button <sam_button_94@hotmail.co.uk>
2022-06-21 11:57:36 +0100

    Green: returns 0 if not string

--- week10/process_scrabble/spec/scrabble.spec.js
+++ week10/process_scrabble/spec/scrabble.spec.js
@@ -2,5 +2,10 @@ const scrabble = require('../lib/scrabble')
 
 describe("scrabble", () => {
   it("returns 0 given an empty string", () => {  
-    expect(scrabble('')).toEqual(0)});
+    expect(scrabble('')).toEqual(0)
+  });
+  
+  it("returns 0 if input is not a string", () => {
+    expect(scrabble(9)).toEqual(0);
+  });
 });
